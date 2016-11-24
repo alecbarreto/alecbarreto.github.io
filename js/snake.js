7aba36c212acaf351677682de7270e66650b0f0a
alecbarreto <abarreto@standrews-de.org>
2016-11-23 19:38:33 -0500

    movin?

--- js/snake.js
+++ js/snake.js
@@ -1,7 +1,7 @@
 var mycanvas = document.getElementById('myCanvas');
 var ctx = mycanvas.getContext('2d');
 var snakeSize = 20;
-var w = 1200;
+var w = 800;
 var h = 800;
 var score = 0;
 var x = 0;
@@ -12,9 +12,10 @@ var food;
 ctx.fillStyle="#FF0000";
 ctx.fillRect(0,0,20,20);
 
-setInterval(move,1);
 function move(canvas,size) {
   canvas.clearRect(x,y,size,size);
   x += size;
   canvas.fillRect(x,y,size,size);
 }
+setInterval(move(canvas,size),1000);
+move(ctx,20);
