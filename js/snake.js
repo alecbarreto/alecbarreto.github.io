96bb143011d927e7d07d27161e0d5fadfc459693
alecbarreto <abarreto@standrews-de.org>
2016-11-23 19:33:54 -0500

    let's get movie

--- js/snake.js
+++ js/snake.js
@@ -1,13 +1,20 @@
-var mycanvas = document.getElementById('canvas');
+var mycanvas = document.getElementById('myCanvas');
 var ctx = mycanvas.getContext('2d');
-var snakeSize = 10;
-var w = 350;
-var h = 350;
+var snakeSize = 20;
+var w = 1200;
+var h = 800;
 var score = 0;
+var x = 0;
+var y = 0;
 var snake;
-var snakeSize = 10;
 var food;
 
 ctx.fillStyle="#FF0000";
 ctx.fillRect(0,0,20,20);
-ctx.fillRect(1180,780,20,20);
+
+setInterval(move,1);
+function move(canvas,size) {
+  canvas.clearRect(x,y,size,size);
+  x += size;
+  canvas.fillRect(x,y,size,size);
+}
